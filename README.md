4132d90eaee3767e6682766448059b1d8158ce5c
alissonRH <alisson.honorato@live.com>
2022-11-15 23:57:44 -0300

    configs

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # map-app
+
+## The postgres container was created using:
+```
+docker run --name=PostGIS -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=geolocation -p 5421:5432 -d postgres          
+
+
+```
+
+docker exec -it PostGIS bash
+
+apt-get update && apt-get install postgis -y
+
+#then login into psql
+
+psql -U admin -h localhost -d geolocation
+
+#then run:
+
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-    host: process.env.DB_HOST || 'localhost',
-    username: process.env.DB_USER || 'admin',
-    password: process.env.DB_PASS || 'admin',
-    port: Number(process.env.DB_PORT) || 5433,
-    database: process.env.DB_NAME || 'geolocation',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
     dialect: 'postgres',
     define: {
       timestamps: true
